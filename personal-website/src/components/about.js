359fc92745b9bcd6d7b71382fa0b95b444855c45
Angela Yang <agyang@andrew.cmu.edu>
2020-12-28 19:07:47 -0500

    add about section and cmu ai click

--- personal-website/src/components/about.js
+++ personal-website/src/components/about.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
       return (
-        <div id="about" className="aboutContainer sectionHeader">
+        <div id="about" className="aboutContainer"> 
+            <div className="sectionHeader">
+                <p>About </p>
+            </div>
             <div className="aboutBody">
-                <p>About</p>
+                <p>
+                    I am a recent graduate from the School of Computer Science at Carnegie Mellon University, where 
+                    I obtained a bachelors in Artificial Intelligence. My academic interests include natural language processing, 
+                    information retrieval, and distributed systems. I am currently a software developer at Benchling. 
+                    In my free time, I enjoy creating digital art, playing video games, and listening to podcasts. Check out my 
+                    blog where I share my thoughts on a variety of topics!  
+                </p>
+                <a href="https://ai.cs.cmu.edu/" target="_blank">
+                    <img className="aboutImage" src ='images/cmu.png' alt="Carnegie Mellon University">
+                    </img>
+                </a>
+                
             </div>
         </div>
         )
